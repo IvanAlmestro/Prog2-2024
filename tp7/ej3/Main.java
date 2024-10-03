c62f28ee25adeaf5d1e57b39f6f39944ca25e28e
Ivan <ivanalmestro@hotmail.com>
2024-10-03 14:34:04 -0300

    se agrego ej con pruebas en el main

--- tp7/ej3/Main.java
+++ tp7/ej3/Main.java
@@ -7,9 +7,17 @@ public class Main {
         
         vivero.addPlanta(lengua);
 
-        CriterioNombreCientifico palabra = new CriterioNombreCientifico("sansi");
+        CriterioNombreCientifico palabra = new CriterioNombreCientifico("Sansi");
+        
+
+        CriterioSolSuperior solSuperiorA2 = new CriterioSolSuperior(2);
+        // criterio para hacer que sea inferior a un numero que le pasamos, hacemos que sea negativo el de superior
+        CriterioNOT solInferiorA2 = new CriterioNOT(solSuperiorA2);
+
+        CriterioAND palabraYSolSuperior = new CriterioAND(solSuperiorA2, palabra);
+        
         System.out.println("-");
-       System.out.println(vivero.buscar(palabra);); 
+       System.out.println(vivero.buscar(palabraYSolSuperior)); 
 
 
     }
